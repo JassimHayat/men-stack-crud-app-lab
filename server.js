1cd393f586c6ba33fae4b4a677dc6c54f33e45ee
Jassim Hayat <jassim.hayat@gmail.com>
2025-01-01 13:43:23 +0300

    men-stack-crud-app-lab

--- server.js
+++ server.js
@@ -9,26 +9,45 @@ const app = express();
 //----------------------MIDDLEWARE-------------------------------------
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true })); // we need to ad this to use the boolean things
+app.use(express.urlencoded({ extended: false })); // we need to ad this to use the boolean things
 //------------------ROUTES-----------------------------------------
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
+
+
 // GET /foods/new
+
 app.get("/foods/new", (req, res) => {
   res.render("foods/new.ejs");
 });
+
 app.get("/foods/:foodId", async (req, res) => {
   const foodId = req.params.foodId;
   const food = await Food.findById(foodId);
   res.render("foods/show.ejs", { food: food });
 });
 
-app.get("/foods/:FoodId/edit", async (req, res) => {
-  const foundFood = await Food.findById(req.params.foodId);
+
+app.get("/foods/:foodId/edit", async (req, res) => {
+  const foundfood = await Food.findById(req.params.foodId);
   res.render("foods/edit.ejs", {
-    food: foundFood,
+    food: foundfood,
   });
+});
+
+app.put('/foods/;foodId', async (req, res) => {
+
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false
+  }
+
+  const foodId = req.params.foodId
+  await Food.findByIdAndUpdate(foodId,req.body);
+  res.redirect(`/foods/${foodId}`);
+
 });
 
 
@@ -38,23 +57,16 @@ app.delete("/foods/:foodId", async (req, res) => {
   res.redirect("/foods");
 });
 
-//POST- it will create/add the foods 
-//Redirect you add the /, the render dont add the slash at the begining
-app.post("/foods", async (req, res) => {
-  if (req.body.isReadyToEat === "on") {
-    req.body.isReadyToEat = true;
-  } else {
-    req.body.isReadyToEat = false;
-  }
-  await Food.create(req.body);
-  res.redirect("/foods/new");
-});
+
+
 // GET /foods
 app.get("/foods", async (req, res) => {
   const allFoods = await Food.find();
   res.render("foods/index.ejs", { foods: allFoods });
 });
 
+
+//POST- it will create/add the foods 
 
 // POST /foods
 app.post("/foods", async (req, res) => {
@@ -68,23 +80,6 @@ app.post("/foods", async (req, res) => {
 });
 
 
-
-
-
-
-
-
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
-
-
-
-
-
-
-
-
-
-
-
